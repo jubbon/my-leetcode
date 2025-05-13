56ed26e4d1c1b44d612b2bb4e7e5d21fc2320d07
Dmitry Kulikov <dmanakulikov@gmail.com>
2025-05-13 09:40:34 +0300

    Added template folder

---
+++ problems/python/_template_/description.md
@@ -0,0 +1,10 @@
+# Task
+
+# Examples
+## Example 1
+
+## Example 2
+
+## Example 3
+
+# Constraints
---
+++ problems/python/_template_/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    pass
+
+def test_solution(input, expected):
+    solution = Solution()
+    assert solution
+
+if __name__ == "__main__":
+    test_solution([], [])
